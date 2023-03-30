f75f1b36da423c52bba5708c7466db5d28d72b4a
Sergey <nesterenko.sa@edu.spbstu.ru>
2023-03-31 00:25:19 +0300

    Initial commit

---
+++ src/main/java/org/example/Grep.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Grep {
+    private final boolean invert;
+    private final boolean ignore;
+    private final boolean regex;
+    private final String word;
+    private final String fileName;
+    public Grep(boolean ignore, boolean invert, boolean regex, String word, String fileName) {
+        this.invert = invert;
+        this.ignore = ignore;
+        this.regex = regex;
+        if (this.ignore)
+            this.word = word.toLowerCase();
+        else
+            this.word = word;
+        this.fileName = fileName;
+    }
+    public void findLines() throws FileNotFoundException {
+        List<String> out = new ArrayList<>();
+        try (FileReader fin = new FileReader(fileName);
+            BufferedReader buffer = new BufferedReader(fin)) {
+            String str;
+            while ((str = buffer.readLine()) != null) {
+                String iStr = ignore ? str.toLowerCase() : str;
+                if (regex) {
+                    Pattern pattern = Pattern.compile(word);
+                    Matcher matcher = pattern.matcher(iStr);
+                    if (!invert && matcher.find())
+                        System.out.println(str);
+                } else {
+                    if (!invert && iStr.contains(word))
+                        System.out.println(str);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка ввода-вывода");
+        }
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.spi.StringArrayOptionHandler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    @Option(name = "-v", usage = "inverts filter condition")
+    private boolean invert;
+
+    @Option(name = "-i", usage = "ignores register")
+    private boolean ignore;
+
+    @Option(name = "-r", usage = "uses regex instead of words")
+    private boolean regex;
+
+    @Argument(handler = StringArrayOptionHandler.class, required = true)
+    private List<String> arguments;
+
+    public static void main(String[] args) throws IOException {
+        new Main().doMain(args);
+    }
+
+    public void doMain(String[] args) throws IOException {
+        CmdLineParser parser = new CmdLineParser(this);
+        String word;
+        String fileName;
+        try {
+            parser.parseArgument(args);
+            word = arguments.get(0);
+            fileName = arguments.get(1);
+        }
+        catch( CmdLineException e ) {
+            // взято с https://github.com/kohsuke/args4j/blob/master/args4j/examples/SampleMain.java
+            System.err.println(e.getMessage());
+            System.err.println("java Main [-r] [-v] [-i] word fileName");
+            parser.printUsage(System.err);
+            return;
+        }
+        System.out.println(fileName);
+        new Grep(ignore, invert, regex, word, fileName).findLines();
+    }
+}
